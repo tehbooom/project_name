22b5c060edddb8b22ceaef7a9b62b4affebce5e1
PR070HAX0r <onlyboomm@gmail.com>
2022-08-20 13:56:03 -0400

    Added function to  intialize datbase

--- model/model.go
+++ model/model.go
@@ -2,9 +2,14 @@ package model
 
 import (
 	"database/sql"
+	"io/ioutil"
 	"log"
 	"math/rand"
+	"net/http"
+	"regexp"
 	"time"
+
+	"github.com/tehbooom/project_name/app"
 )
 
 type Name struct {
@@ -64,4 +69,69 @@ func (n *Name) Getwords(DB *sql.DB) ([]Name, error) {
 		return names, nil
 	}
 
+}
+
+const nurl = "https://greenopolis.com/list-of-nouns/"
+const aurl = "https://greenopolis.com/adjectives-list/"
+
+func (a *app.App) initializeDB() {
+
+	// regex
+	re := regexp.MustCompile(`<li>(.*?)</li>`)
+
+	// create table
+	const table = ` CREATE TABLE [IF NOT EXISTS] words (
+		id serial PRIMARY KEY,
+		noun text
+		adjective text
+		
+	)`
+	a.DB.Exec(table)
+
+	// nouns
+
+	const nrow = `INSERT INTO words (
+	noun
+	)
+	VALUES $1
+	)`
+
+	nresp, err := http.Get(nurl) // get contents of noun webpage
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nresp.Body.Close()
+	nhtml, err := ioutil.ReadAll(nresp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	nmatches := re.FindAllStringSubmatch(string(nhtml), -1) // select all words from regex and insert into table
+	for _, w := range nmatches {
+		a.DB.Exec(nrow, w[1])
+	}
+	defer a.DB.Close()
+
+	//adjectives
+	const arow = `INSERT INTO words (
+		adjective
+	)
+	VALUES $1
+	)`
+
+	aresp, err := http.Get(aurl) // get contents of adjectives webpage
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer aresp.Body.Close()
+	ahtml, err := ioutil.ReadAll(aresp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	amatches := re.FindAllStringSubmatch(string(ahtml), -1) // select all words from regex and insert into table
+	for _, w := range amatches {
+		a.DB.Exec(arow, w[1])
+	}
+	defer a.DB.Close()
 }
