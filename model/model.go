7a112f92fcc71eb43bc8aff5e42c17d9c287ee92
PR070HAX0r <onlyboomm@gmail.com>
2022-08-21 13:01:10 -0400

    created two tables for db instead of one

--- model/model.go
+++ model/model.go
@@ -2,14 +2,9 @@ package model
 
 import (
 	"database/sql"
-	"io/ioutil"
 	"log"
 	"math/rand"
-	"net/http"
-	"regexp"
 	"time"
-
-	"github.com/tehbooom/project_name/app"
 )
 
 type Name struct {
@@ -21,14 +16,14 @@ var projectName string
 func (n *Name) Getwords(DB *sql.DB) ([]Name, error) {
 	var err error
 	var aNum int
-	aRows := DB.QueryRow("SELECT COUNT (DISTINCT adjective) FROM words WHERE adjective IS NOT NULL")
+	aRows := DB.QueryRow("SELECT COUNT (DISTINCT word) FROM adjectives WHERE word IS NOT NULL")
 	err = aRows.Scan(&aNum)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	var nNum int
-	nRows := DB.QueryRow("SELECT COUNT (DISTINCT noun) FROM words WHERE noun IS NOT NULL")
+	nRows := DB.QueryRow("SELECT COUNT (DISTINCT word) FROM nouns WHERE word IS NOT NULL")
 	err = nRows.Scan(&nNum)
 	if err != nil {
 		log.Fatal(err)
@@ -39,7 +34,7 @@ func (n *Name) Getwords(DB *sql.DB) ([]Name, error) {
 
 	var adjective string
 	aRand := rand.Intn(aNum-min+1) + min
-	aWord := DB.QueryRow("SELECT adjective FROM words where id=$1", aRand)
+	aWord := DB.QueryRow("SELECT word FROM adjectives where id=$1", aRand)
 	err = aWord.Scan(&adjective)
 	if err != nil {
 		log.Fatal(err)
@@ -47,7 +42,7 @@ func (n *Name) Getwords(DB *sql.DB) ([]Name, error) {
 
 	var noun string
 	nRand := rand.Intn(nNum-min+1) + min
-	nWord := DB.QueryRow("SELECT noun FROM words where id=$1", nRand)
+	nWord := DB.QueryRow("SELECT word FROM nouns where id=$1", nRand)
 	err = nWord.Scan(&noun)
 	if err != nil {
 		log.Fatal(err)
@@ -60,7 +55,7 @@ func (n *Name) Getwords(DB *sql.DB) ([]Name, error) {
 	p := &projectName
 
 	if p == &tempProjectName {
-		return names, err
+		return names, err //need to restart the stuff
 	} else {
 		var n Name
 		projectname := tempProjectName
@@ -69,69 +64,4 @@ func (n *Name) Getwords(DB *sql.DB) ([]Name, error) {
 		return names, nil
 	}
 
-}
-
-const nurl = "https://greenopolis.com/list-of-nouns/"
-const aurl = "https://greenopolis.com/adjectives-list/"
-
-func (a *app.App) initializeDB() {
-
-	// regex
-	re := regexp.MustCompile(`<li>(.*?)</li>`)
-
-	// create table
-	const table = ` CREATE TABLE [IF NOT EXISTS] words (
-		id serial PRIMARY KEY,
-		noun text
-		adjective text
-		
-	)`
-	a.DB.Exec(table)
-
-	// nouns
-
-	const nrow = `INSERT INTO words (
-	noun
-	)
-	VALUES $1
-	)`
-
-	nresp, err := http.Get(nurl) // get contents of noun webpage
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer nresp.Body.Close()
-	nhtml, err := ioutil.ReadAll(nresp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	nmatches := re.FindAllStringSubmatch(string(nhtml), -1) // select all words from regex and insert into table
-	for _, w := range nmatches {
-		a.DB.Exec(nrow, w[1])
-	}
-	defer a.DB.Close()
-
-	//adjectives
-	const arow = `INSERT INTO words (
-		adjective
-	)
-	VALUES $1
-	)`
-
-	aresp, err := http.Get(aurl) // get contents of adjectives webpage
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer aresp.Body.Close()
-	ahtml, err := ioutil.ReadAll(aresp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	amatches := re.FindAllStringSubmatch(string(ahtml), -1) // select all words from regex and insert into table
-	for _, w := range amatches {
-		a.DB.Exec(arow, w[1])
-	}
-	defer a.DB.Close()
 }
